965d5d420e14d79024856e9ccc8bc0f3fc2b5aad
Nicholas Harris <nharris614@gmail.com>
2019-12-09 20:59:42 -0600

    Completed logic for sorting plants

--- src/lib/fetchPlants.js
+++ src/lib/fetchPlants.js
@@ -4,32 +4,38 @@ function getPlants(selectedDate) {
     const endTime = 1583798040000;
     let waterToday = [];
     const diffTime = Math.abs(endTime - selectedDate);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const dayOfWk = selectedDate.getDay();
     
     //takes in a date, and returns the plant(s) that need to be watered on the given day. 
     const daysPassed = 84 - diffDays;
 
-    //
-    const isWeekend = selectedDate.getDay() === (5 || 6) ? true : false;
+    //determines if it's a weekend or not 
+    const isWeekend = dayOfWk === 0 || dayOfWk === 6 ? true : false;
 
     /// loop through the plants to get water after days, then evaluate: 
     for (let i = 0; i <= plants.length - 1; i++) {
         let waterAfter = plants[i].water_after.split("").splice(0, 2).join("");
-        console.log("Water", plants[i].name, "after", waterAfter, "days");
+        let plantName = plants[i].name
+        console.log("Water", plantName, "after", waterAfter, "days");
         
-        // if (daysPassed % waterAfter === 0 && !isWeekend) {
-        //     waterToday.push(/*currentplantname*/)
-        // } else if (/* take care of weekend cases here */ ) {
-        //     waterToday.push(/*currentplantname*/)
-        // }
+        if (daysPassed === 1) {
+            //no watering on day 1
+            waterToday = [];
+        } else if (daysPassed % waterAfter === 0 && !isWeekend) {
+            //normal watering case
+            waterToday.push(plantName)
+        } else if (daysPassed % waterAfter === 1 && !isWeekend) {
+            // picks up the plants to be watered every three days
+            if (waterAfter % 3 === 0) {
+                waterToday.push(plantName)
+            }
+        } 
     }
-    
+    // daysPassed % waterAfter === 1 && (daysPassed + 1) % waterAfter !== 0 && (daysPassed - 1) % waterAfter !== 0
     //console logs for testing 
-    console.log("Plant array:", plants);
-    console.log("Selected date", selectedDate);
-    console.log("Days between selected date and end date", diffDays);
-    console.log("Time of selected date", selectedDate.getTime());
-    console.log(daysPassed, "days have passed since scheduler began");
+    console.log("To water today:", waterToday)
+    return waterToday;
 }
 
 module.exports = getPlants;
