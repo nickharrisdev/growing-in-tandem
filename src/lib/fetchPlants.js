9ede4325ec4e5c4b692d14e5a66d1a0f7d7c4621
Nicholas Harris <nharris614@gmail.com>
2019-12-15 18:40:50 -0600

    Added logic to accommodate for user added plants

--- src/lib/fetchPlants.js
+++ src/lib/fetchPlants.js
@@ -15,7 +15,8 @@ export function getPlants(selectedDate) {
     const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
     const dayOfWk = selectedDate.getDay();
     const daysPassed = 84 - diffDays;
-
+    const isWeekend = (dayOfWk === 0 || dayOfWk === 6) ? true : false;
+    
     /// loop through the plants to get water after days, then evaluate: 
     for (let i = 0; i <= plants.length - 1; i++) {
         let waterAfter = plants[i].water_after.split("").splice(0, 2).join("");
@@ -27,6 +28,15 @@ export function getPlants(selectedDate) {
         } else if ((dayOfWk === 3 || dayOfWk === 5 || dayOfWk === 1) && waterAfterNum === 2) {
             waterToday.push(plantName);
         } else if ((dayOfWk === 4 || dayOfWk === 1) && waterAfterNum === 3) {
+            waterToday.push(plantName);
+        } else if ((dayOfWk === 1) && (daysPassed % waterAfterNum === 1) && waterAfterNum !== 2 && waterAfterNum !== 3) {
+            //catches plants that were supposed to be watered on sunday
+            waterToday.push(plantName);
+        } else if (daysPassed % waterAfterNum === 0 && (waterAfterNum !== 2 && waterAfterNum !== 3) && !(Number.isInteger(waterAfterNum / 7)) && !isWeekend) {
+            //ensures plants added to original array get watered on correct days, while excluding original plants. 
+            waterToday.push(plantName);
+        } else if ((dayOfWk === 5) && (daysPassed % waterAfterNum === waterAfterNum - 1) && waterAfterNum !== 2 && waterAfterNum !== 3) {
+            //catches plants that were supposed to be watered on saturday.
             waterToday.push(plantName);
         }
     }
