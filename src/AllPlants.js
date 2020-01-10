7d164622f433a1ccbdd81fff969f9589d30baf3e
Nicholas Harris <nharris614@gmail.com>
2020-01-10 00:36:10 -0600

    Bug Fix

--- src/AllPlants.js
+++ src/AllPlants.js
@@ -3,7 +3,6 @@ import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { Link, withRouter } from 'react-router-dom';
 
